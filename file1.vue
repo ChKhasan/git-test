b18ee3cb0934d2cc6920106acd0eeec27b41bccf
Khasan_Ch <chulliyevhasan1405@mail.ru>
2024-01-05 15:48:33 +0500

    text2

--- file1.vue
+++ file1.vue
@@ -2,12 +2,12 @@
 
 <template lang="html">
   <div>
-    <h1>{{$store.state.translations['file1.0']}}</h1>
-    <p>{{$store.state.translations['file1.1']}}</p>
+    <h1 class="classqul">{{ $store.state.translations["file1.0"] }}</h1>
+    <p>{{ $store.state.translations["file1.1"] }}</p>
     <ul>
-      <li>{{$store.state.translations['file1.2']}}</li>
-      <li>{{$store.state.translations['file1.3']}}</li>
-      <li>{{$store.state.translations['file1.4']}}</li>
+      <li>{{ $store.state.translations["file1.2"] }}</li>
+      <li>{{ $store.state.translations["file1.3"] }}</li>
+      <li>{{ $store.state.translations["file1.4"] }}</li>
     </ul>
   </div>
 </template>
