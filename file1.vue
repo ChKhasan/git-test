2376188046ccf5710fbd794ab277806fde910216
Khasan_Ch <chulliyevhasan1405@mail.ru>
2024-01-05 16:14:20 +0500

    test_branch_commit

--- file1.vue
+++ file1.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="class qul qweuqwie">{{$store.state.translations['file1.0']}}</h1>
     <p>{{$store.state.translations['file1.1']}}</p>
-    <ul>
+    <ul class="test_branch">
       <li>{{ $store.state.translations["file1.2"] }}</li>
       <li>{{ $store.state.translations["file1.3"] }}</li>
       <li>{{ $store.state.translations["file1.4"] }}</li>
