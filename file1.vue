d9d013c02fb51188a4161da3a6c32da884646967
mikamidies <mikamidies04@gmail.com>
2024-01-05 15:44:48 +0500

    Test commit

--- file1.vue
+++ file1.vue
@@ -1,23 +1,17 @@
-<!-- SimpleComponent.vue -->
-
-<template lang="html">
+<template>
   <div>
-    <h1>{{$store.state.translations['file1.0']}}</h1>
-    <p>{{$store.state.translations['file1.1']}}</p>
+    <h1 class="changed">{{ $store.state.translations["file1.0"] }}</h1>
+    <p>{{ $store.state.translations["file1.1"] }}</p>
     <ul>
-      <li>{{$store.state.translations['file1.2']}}</li>
-      <li>{{$store.state.translations['file1.3']}}</li>
-      <li>{{$store.state.translations['file1.4']}}</li>
+      <li>{{ $store.state.translations["file1.2"] }}</li>
+      <li>{{ $store.state.translations["file1.3"] }}</li>
+      <li>{{ $store.state.translations["file1.4"] }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  // No data properties in this example
-};
+export default {};
 </script>
 
-<style scoped>
-/* Your component styles here */
-</style>
+<style scoped></style>
